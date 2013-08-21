c837d14e933114f543dd3931ac644bade97c19b8
Brian E. Granger <ellisonbg@gmail.com>
2013-08-21 14:55:47 -0700

    Adding tutorial about citations.

--- citations/ipython_nbconvert_config.py
+++ citations/ipython_nbconvert_config.py
@@ -1,7 +1,7 @@
 c = get_config()
 
 #Export all the notebooks in the current directory to the sphinx_howto format.
-c.NbConvertApp.notebooks = ['*.ipynb']
+c.NbConvertApp.notebooks = ['Tools for the lifecycle of computational research.ipynb']
 c.NbConvertApp.export_format = 'latex'
 c.Exporter.template_file = 'citations'
 c.SphinxTransformer.author = 'Fernando Perez and Brian E. Granger'
