11e79bce478bf07f15a65482de2cb9b8698f4b4a
Jonathan Frederic <jdfreder@calpoly.edu>
2013-08-27 16:33:33 -0700

    Updated class reference

--- custom_preprocessor/ipython_nbconvert_config.py
+++ custom_preprocessor/ipython_nbconvert_config.py
@@ -2,4 +2,4 @@ c = get_config()
 
 #Export all the notebooks in the current directory to the sphinx_howto format.
 c.NbConvertApp.notebooks = ['*.ipynb']
-c.Exporter.transformers = ['cheese_transformer.CheeseTransformer']
+c.Exporter.transformers = ['cheese_preprocessor.CheesePreprocessor']
