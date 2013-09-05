6da514e324a06b2f2a12006b50a273132147dc9f
Jonathan Frederic <jdfreder@calpoly.edu>
2013-09-05 09:49:56 -0700

    Added pdf post processor, so `ipython nbconvert` will actually work

--- citations/ipython_nbconvert_config.py
+++ citations/ipython_nbconvert_config.py
@@ -3,4 +3,6 @@ c = get_config()
 #Export all the notebooks in the current directory to the sphinx_howto format.
 c.NbConvertApp.notebooks = ['Tools for the lifecycle of computational research.ipynb']
 c.NbConvertApp.export_format = 'latex'
+c.NbConvertApp.postprocessor_class = 'PDF'
+
 c.Exporter.template_file = 'citations'
