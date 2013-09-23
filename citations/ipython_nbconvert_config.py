715c853fc850373dc26fa543ed2a28d8ce4d07e8
Brian E. Granger <ellisonbg@gmail.com>
2013-09-23 14:49:03 -0700

    Updating citations example

--- citations/ipython_nbconvert_config.py
+++ citations/ipython_nbconvert_config.py
@@ -1,7 +1,7 @@
 c = get_config()
 
 #Export all the notebooks in the current directory to the sphinx_howto format.
-c.NbConvertApp.notebooks = ['Tools for the lifecycle of computational research.ipynb']
+c.NbConvertApp.notebooks = ['LifecycleTools.ipynb']
 c.NbConvertApp.export_format = 'latex'
 c.NbConvertApp.postprocessor_class = 'PDF'
 
